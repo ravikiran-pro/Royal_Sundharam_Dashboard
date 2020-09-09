0fe1792d7f1d5930e878f18619798420cf0d4bb3
ravikiran-pro <proraviki@gmail.com>
2020-09-10 05:21:54 +0530

    Pending relative path failed ----map responsive not working

--- assets/js/googlemap.js
+++ assets/js/googlemap.js
@@ -1,37 +1,25 @@
 google.charts.load('current', {
-    'packages':['geochart'],
-    // Note: you will need to get a mapsApiKey for your project.
-    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-  });
-  google.charts.setOnLoadCallback(drawRegionsMap);
+  'packages':['geochart'],
+  // Note: you will need to get a mapsApiKey for your project.
+  // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+  'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
 
-  function drawRegionsMap() {
-    var data = google.visualization.arrayToDataTable([
-      ['Country', 'Leads'],
-      ['Germany', 200],
-      ['United States', 300],
-      ['Brazil', 400],
-      ['Canada', 500],
-      ['France', 600],
-      ['RU', 700]
-    ]);
+function drawRegionsMap() {
+  var data = google.visualization.arrayToDataTable([
+    ['Country', 'Popularity'],
+    ['Germany', 200],
+    ['United States', 300],
+    ['Brazil', 400],
+    ['Canada', 500],
+    ['France', 600],
+    ['RU', 700]
+  ]);
 
-    var options = {
-      colorAxis: {colors: ['#FBB23B']},  
-    };
+  var options = {};
 
-    var chart = new google.visualization.GeoChart(document.getElementById('map-chart'));
+  var chart = new google.visualization.GeoChart(document.getElementById('map-chart'));
 
-    chart.draw(data, options);
-    google.visualization.events.addListener(chart, 'onclick', function(e) {
-      setTooltipContent(dataTable,e.row);
-  });
-  }
-  function setTooltipContent(dataTable,row) {
-    if (row != null) {
-        var content = '<div class="custom-tooltip" ><h1>' + dataTable.getValue(row, 0) + '</h1><div>' + dataTable.getValue(row, 1) + '</div></div>'; //generate tooltip content
-        var tooltip = document.getElementsByClassName("google-visualization-tooltip")[0];
-        tooltip.innerHTML = content;
-    }
+  chart.draw(data, options);
 }
--- assets/js/jqwidgets.js
+++ assets/js/jqwidgets.js
@@ -11,60 +11,30 @@ function GenerateWidth(){
 
 function SetupJqWidgets(){
     var data = new Array();
-    
     // create a data source and data adapter
-    var Header=['Opening os claim','live count','claims paid','xol cost','live count','claims unpaid'];
-    
-    // data[0]={'HeaderCost':'Opening Os Claim','productname':'DateRange','ActualTotalCost':0,'EstimatedBudgetCost':0};
-    // data[1]={'HeaderCost':'Opening Os Claim','SubCost':'claim','productname':'DateRange','ActualTotalCost':3,'EstimatedBudgetCost':4};
-    // data[2]={'HeaderCost':'Opening Os Claim','SubCost':'claim1','productname':'DateRange','price':3,'ActualTotalCost':8,'EstimatedBudgetCost':10};
-    
-    // data[3]={'HeaderCost':'Opening Os Claim','productname':'DateRange1','ActualTotalCost':0,'EstimatedBudgetCost':0};
-    // data[4]={'HeaderCost':'Opening Os Claim','SubCost':'claim','productname':'DateRange1','ActualTotalCost':3,'EstimatedBudgetCost':4};
-    // data[5]={'HeaderCost':'Opening Os Claim','SubCost':'claim1','productname':'DateRange1','price':3,'ActualTotalCost':8,'EstimatedBudgetCost':10};
-    
-    // data[6]={'HeaderCost':'Opening Os Claim','productname':'DateRange2','ActualTotalCost':0,'EstimatedBudgetCost':10};
-    // data[7]={'HeaderCost':'Opening Os Claim','SubCost':'claim','productname':'DateRange2','ActualTotalCost':3,'EstimatedBudgetCost':4};
-    // data[8]={'HeaderCost':'Opening Os Claim','SubCost':'claim1','productname':'DateRange2','price':3,'ActualTotalCost':8,'EstimatedBudgetCost':4};
-
-    // data[9]={'HeaderCost':'Opening Os Claim','productname':'DateRange','ActualTotalCost':0,'EstimatedBudgetCost':0};
-    // data[10]={'HeaderCost':'Opening Os Claim','SubCost':'claim','productname':'DateRange','ActualTotalCost':3,'EstimatedBudgetCost':4};
-    // data[11]={'HeaderCost':'Opening Os Claim','SubCost':'claim1','productname':'DateRange','price':3,'ActualTotalCost':8,'EstimatedBudgetCost':10};
-    
-    // data[12]={'HeaderCost':'change','productname':'DateRange1','ActualTotalCost':0,'EstimatedBudgetCost':0};
-    // data[13]={'HeaderCost':'change','SubCost':'claim','productname':'DateRange1','ActualTotalCost':3,'EstimatedBudgetCost':4};
-    // data[14]={'HeaderCost':'change','SubCost':'claim1','productname':'DateRange1','price':3,'ActualTotalCost':8,'EstimatedBudgetCost':10};
-    
-    // data[15]={'HeaderCost':'change','productname':'DateRange2','ActualTotalCost':0,'EstimatedBudgetCost':10};
-    // data[15]={'HeaderCost':'change','SubCost':'claim','productname':'DateRange2','ActualTotalCost':3,'EstimatedBudgetCost':4};
-    // data[16]={'HeaderCost':'change','SubCost':'claim1','productname':'DateRange2','price':3,'ActualTotalCost':8,'EstimatedBudgetCost':4};
-    var firstNames =
+    ///Sample Mock Data
+    var Header =
     [
-        "Andrew", "Nancy", "Shelley", "Regina", "Yoshi", "Antoni", "Mayumi", "Ian", "Peter", "Lars", "Petra", "Martin", "Sven", "Elio", "Beate", "Cheryl", "Michael", "Guylene"
-    ];
-    var lastNames =
-    [
-        "Fuller", "Davolio", "Burke", "Murphy", "Nagase", "Saavedra", "Ohno", "Devling", "Wilson", "Peterson", "Winkler", "Bein", "Petersen", "Rossi", "Vileid", "Saylor", "Bjorn", "Nodier"
+        "Opening OS Claim","Live Cost","Claim Part","XDL Cost","Live Count","Claims Paid","Operating Cost",
     ];
     var productNames =
     [
-        "Black Tea", "Green Tea", "Caffe Espresso", "Doubleshot Espresso", "Caffe Latte", "White Chocolate Mocha", "Cramel Latte", "Caffe Americano", "Cappuccino", "Espresso Truffle", "Espresso con Panna", "Peppermint Mocha Twist"
+        "DateRange1","DateRange2","DateRange3"
     ];
     var priceValues =
     [
-        "2.25", "1.5", "3.0", "3.3", "4.5", "3.6", "3.8", "2.5", "5.0", "1.75", "3.25", "4.0"
+        "2000025", "100005", "300000", "300003", "400005", "300006", "300008", "200005", "500000", "1000075", "3000025", "400000"
     ];
     for (var i = 0; i < 500; i++) {
         var row = {};
-        var productindex = Math.floor(Math.random() * productNames.length);
-        var price = parseFloat(priceValues[productindex]);
+        var productindex = Math.floor(Math.random() * Header.length);
+        var price = Math.round(Math.random() * 10);
         var quantity = 1 + Math.round(Math.random() * 10);
-        row["HeaderCost"] = firstNames[Math.floor(Math.random() * 10)];
-        row["SubCost"] = lastNames[Math.floor(Math.random() * lastNames.length)];
+        row["HeaderCost"] = Header[Math.floor(Math.random() * Header.length)];
+        row["SubCost"] = "claim"+Math.floor(Math.random() * Header.length);
         row["productname"] = productNames[ Math.floor(Math.random() * 3)];
-        row["ActualTotalCost"] = price;
+        row["ActualTotalCost"] = priceValues[Math.floor(Math.random()*3)];
         row["EstimatedBudgetCost"] = quantity;
-        row["total"] = price * quantity;
         data[i] = row;
     }
     
@@ -92,8 +62,8 @@ function SetupJqWidgets(){
             rows: [{ dataField: 'HeaderCost',width:width}, { dataField: 'SubCost'}],
             columns: [{ dataField: 'productname',width:width*2}],
             values: [
-                { dataField: 'ActualTotalCost',width:width,'function': 'sum', text: 'Actual', formatSettings: { prefix: '$', decimalPlaces: 2} },
-                { dataField: 'EstimatedBudgetCost',width:width,'function':'sum', text: 'Budget', formatSettings: { prefix: '$', decimalPlaces: 2 } }
+                { dataField: 'ActualTotalCost',width:width,'function': 'sum', text: 'Actual', formatSettings: { prefix: '', decimalPlaces: 0} },
+                { dataField: 'EstimatedBudgetCost',width:width,'function':'sum', text: 'Budget', formatSettings: { prefix: '', decimalPlaces: 0 } }
             ]
         });
     // create a pivot grid
